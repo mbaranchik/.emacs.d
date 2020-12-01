915e8f001482aace193da11e38cc4e0a6e2bcadd
Michael Baranchik <mbaranch@amazon.com>
2020-12-01 08:14:32 +0200

    Fix whitespace mode for prog-mode only

--- whitespace.el
+++ whitespace.el
@@ -6,7 +6,11 @@
 (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
 
 (autoload 'whitespace-mode "whitespace" "Toggle whitespace visualization." t)
-(global-whitespace-mode 1)
+(add-hook 'prog-mode-hook 'whitespace-mode)
+;;(add-hook 'vterm-mode-hook (lambda ()
+;;                             (whitespace-mode nil)
+;;                             )
+;;          )
 
 (use-package indent-guide)
 (when use-indent-guide
