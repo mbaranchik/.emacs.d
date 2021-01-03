968392ebbf9aa82fa11338cb5018ee000f42e801
Michael Baranchik <mbaranch@amazon.com>
2021-01-03 21:23:09 +0200

    Add centaur-tabs

--- navigation.el
+++ navigation.el
@@ -12,7 +12,7 @@
 ;;  )
 
 ;; Enable Tab-Line Mode (Emacs >= 27)
-(global-tab-line-mode t)
+;;(global-tab-line-mode t)
 
 (use-package multiple-cursors
   :commands multiple-cursors-mode)
@@ -26,4 +26,28 @@
 (use-package back-button
   :config
   (back-button-mode 1))
+
+(message "Display: %s" (display-graphic-p))
+(message "Window-System: %s" window-system)
+
+;; Centaur-Tabs
+(daemon-wrap my/load-centaur
+ (if (display-graphic-p)
+     (progn
+       (use-package centaur-tabs
+         :demand
+         :bind
+         ([M-left] . centaur-tabs-backward)
+         ([M-right] . centaur-tabs-forward))
+       (centaur-tabs-mode nil)
+       (setq centaur-tabs-style "bar"
+	         centaur-tabs-height 32
+	         centaur-tabs-set-icons t
+	         centaur-tabs-set-bar 'left
+	         x-underline-at-descent-line t)
+       (centaur-tabs-mode t)
+       (centaur-tabs-headline-match)
+       )
+   )
+ )
 
