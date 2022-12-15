0ce1fc5da1bd5a82cf795fd86f7e640a044c2c0f
Michael Baranchik <mbaranch@amazon.com>
2022-12-15 09:55:17 +0200

    Bind COMMIT_MSG buffers to git mode

--- git.el
+++ git.el
@@ -5,5 +5,9 @@
 (use-package magit
   :commands (magit magit-status))
 
+(use-package gerrit
+  :commands (magit magit-status gerrit-dashboard gerrit-upload gerrit-download)
+  )
 
+(add-to-list 'auto-mode-alist '("/\.git/COMMIT_EDITMSG\\'" . vc-git-log-edit-mode))
 
