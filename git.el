0b941c76d87a5e97593519440987605e9fbf979c
Michael Baranchik <mbaranch@amazon.com>
2022-12-21 10:48:04 +0200

    Use magit-gerrit plugin

--- git.el
+++ git.el
@@ -3,11 +3,13 @@
 ;; TODO - revisit git gutter when stable
 
 (use-package magit
-  :commands (magit magit-status vc-git-log-edit-mode))
-
-(use-package gerrit
-  :commands (magit magit-status gerrit-dashboard gerrit-upload gerrit-download)
+  :mode ("/COMMIT_EDITMSG\\'" . vc-git-log-edit-mode)
+  :commands (magit magit-status vc-git-log-edit-mode)
+  :config
+  (use-package magit-gerrit
+    :ensure t
+    )
   )
 
-(add-to-list 'auto-mode-alist '("/COMMIT_EDITMSG\\'" . vc-git-log-edit-mode))
+;; (add-to-list 'auto-mode-alist '("/COMMIT_EDITMSG\\'" . vc-git-log-edit-mode))
 
