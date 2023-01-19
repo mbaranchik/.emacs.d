c9f59ecf81a43324aafec892c8fa0afe45ab4e1f
Michael Baranchik <mbaranch@amazon.com>
2023-01-19 08:20:30 +0200

    Add git-gutter + git-gutter-fringe

--- git.el
+++ git.el
@@ -11,5 +11,21 @@
     )
   )
 
+(defun my/load-git-gutter ()
+  "loads git-gutter-mode if file has a vc-backend"
+  (when (string= "Git" (vc-backend (buffer-file-name)))
+    (git-gutter-mode)))
+
+(use-package git-gutter
+  :hook (prog-mode . my/load-git-gutter)
+  :config
+  (setq git-gutter:update-interval 0))
+
+(use-package git-gutter-fringe
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+
 ;; (add-to-list 'auto-mode-alist '("/COMMIT_EDITMSG\\'" . vc-git-log-edit-mode))
 
