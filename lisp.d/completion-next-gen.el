2fccbfaa185175efb8c1dbb9b0d6ad3b7a980f10
Michael Baranchik <mbaranch@amazon.com>
2023-10-22 18:10:29 +0300

    Avoid posframe in terminal

--- lisp.d/completion-next-gen.el
+++ lisp.d/completion-next-gen.el
@@ -55,7 +55,9 @@
 (use-package vertico-posframe
   :after vertico
   :config
-  (vertico-posframe-mode))
+  (daemon-wrap my/load-vertico-posframe
+               (vertico-posframe-mode)
+               ))
 
 ;; Persist history over Emacs restarts. Vertico sorts by history position.
 (use-package savehist
